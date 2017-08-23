86c77a4207218e9b4f50422dad23e7ffb2492e38
Austin <asmusaustin@gmail.com>
2017-08-22 22:47:07 -0500

    1.3.1- removed instanceof player check so that console could run command

--- src/com/github/aasmus/pvptoggle/PvPCommand.java
+++ src/com/github/aasmus/pvptoggle/PvPCommand.java
@@ -12,9 +12,6 @@ public class PvPCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(sender instanceof Player)) {
-			return false;
-		}
 		if(cmd.getName().equalsIgnoreCase("pvp")) {
 			Player p = (Player) sender;
 			if(args.length == 0) {
