be4590b7ad30918bac6afa8da0cec53c4b30292f
Retrontology <Retrontology@hotmail.com>
2020-06-28 19:00:33 -0500

    Added PVPToggleEvent

--- src/main/java/com/github/aasmus/pvptoggle/PvPToggle.java
+++ src/main/java/com/github/aasmus/pvptoggle/PvPToggle.java
@@ -11,6 +11,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.aasmus.pvptoggle.events.PVPToggleEvent;
 import com.github.aasmus.pvptoggle.listeners.PlayerJoin;
 import com.github.aasmus.pvptoggle.listeners.PlayerLeave;
 import com.github.aasmus.pvptoggle.listeners.PvP;
